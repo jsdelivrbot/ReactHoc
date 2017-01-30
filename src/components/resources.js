2f15bb6a1c270811ddb6efa3e71701c6a770019c
Paolo Basso <paolo.basso87@gmail.com>
2017-01-30 17:53:25 +0100

    Header, action creator

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default () => {
+
+  return (
+    <div>
+      Super special recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Pepper</li>
+        <li>1 Cup Salt</li>
+      </ul>
+    </div>
+  );
+};
